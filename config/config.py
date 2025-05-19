538b54c8ee504d21202e2eab3c2dcdd9b5088716
prrlesmac <prrlesmac@gmail.com>
2025-05-19 00:00:11 -0600

    new cofnig files

--- config/config.py
+++ config/config.py
@@ -20,3 +20,13 @@ club_name_mapping = {
     "Tottenham": "Tottenham",
     "Brighton": "Brighton",
 }
+number_of_simulations = 10000
+
+elo_output_file = "data/02_intermediate/current_elo_ratings.csv"
+fixtures_output_file = "data/01_raw/epl_matches.csv"
+sim_output_file = "data/03_output/season_standings_sim.csv"
+
+elo_rating_url = "http://api.clubelo.com/2025-04-01"
+fixtures_url = (
+    "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures"
+)
