a1ea6aecba7ce46be81e61c1635d439fe90eae8e
prrlesmac <prrlesmac@gmail.com>
2026-01-19 22:19:57 -0600

    setup mlb configs

--- src/db/sql/mlb_teams.sql
+++ src/db/sql/mlb_teams.sql
@@ -1,48 +1,48 @@
 CREATE TABLE teams_mlb (
     team VARCHAR(50) PRIMARY KEY,
-    conference VARCHAR(50),
-    league VARCHAR(50)
+    division VARCHAR(50),
+    conference VARCHAR(50)
 );
 
 INSERT INTO teams_mlb (team, division, conference) VALUES
 -- American League - East Division
-('Baltimore Orioles', 'East', 'American League'),
-('Boston Red Sox', 'East', 'American League'),
-('New York Yankees', 'East', 'American League'),
-('Tampa Bay Rays', 'East', 'American League'),
-('Toronto Blue Jays', 'East', 'American League'),
+('Baltimore Orioles', 'AL East', 'American League'),
+('Boston Red Sox', 'AL East', 'American League'),
+('New York Yankees', 'AL East', 'American League'),
+('Tampa Bay Rays', 'AL East', 'American League'),
+('Toronto Blue Jays', 'AL East', 'American League'),
 
 -- American League - Central Division
-('Chicago White Sox', 'Central', 'American League'),
-('Cleveland Guardians', 'Central', 'American League'),
-('Detroit Tigers', 'Central', 'American League'),
-('Kansas City Royals', 'Central', 'American League'),
-('Minnesota Twins', 'Central', 'American League'),
+('Chicago White Sox', 'AL Central', 'American League'),
+('Cleveland Guardians', 'AL Central', 'American League'),
+('Detroit Tigers', 'AL Central', 'American League'),
+('Kansas City Royals', 'AL Central', 'American League'),
+('Minnesota Twins', 'AL Central', 'American League'),
 
 -- American League - West Division
-('Houston Astros', 'West', 'American League'),
-('Los Angeles Angels', 'West', 'American League'),
-('Oakland Athletics', 'West', 'American League'),
-('Seattle Mariners', 'West', 'American League'),
-('Texas Rangers', 'West', 'American League'),
+('Houston Astros', 'AL West', 'American League'),
+('Los Angeles Angels', 'AL West', 'American League'),
+('Athletics', 'AL West', 'American League'),
+('Seattle Mariners', 'AL West', 'American League'),
+('Texas Rangers', 'AL West', 'American League'),
 
 -- National League - East Division
-('Atlanta Braves', 'East', 'National League'),
-('Miami Marlins', 'East', 'National League'),
-('New York Mets', 'East', 'National League'),
-('Philadelphia Phillies', 'East', 'National League'),
-('Washington Nationals', 'East', 'National League'),
+('Atlanta Braves', 'NL East', 'National League'),
+('Miami Marlins', 'NL East', 'National League'),
+('New York Mets', 'NL East', 'National League'),
+('Philadelphia Phillies', 'NL East', 'National League'),
+('Washington Nationals', 'NL East', 'National League'),
 
 -- National League - Central Division
-('Chicago Cubs', 'Central', 'National League'),
-('Cincinnati Reds', 'Central', 'National League'),
-('Milwaukee Brewers', 'Central', 'National League'),
-('Pittsburgh Pirates', 'Central', 'National League'),
-('St. Louis Cardinals', 'Central', 'National League'),
+('Chicago Cubs', 'NL Central', 'National League'),
+('Cincinnati Reds', 'NL Central', 'National League'),
+('Milwaukee Brewers', 'NL Central', 'National League'),
+('Pittsburgh Pirates', 'NL Central', 'National League'),
+('St. Louis Cardinals', 'NL Central', 'National League'),
 
 -- National League - West Division
-('Arizona D''Backs', 'West', 'National League'),
-('Colorado Rockies', 'West', 'National League'),
-('Los Angeles Dodgers', 'West', 'National League'),
-('San Diego Padres', 'West', 'National League'),
-('San Francisco Giants', 'West', 'National League');
+('Arizona D''Backs', 'NL West', 'National League'),
+('Colorado Rockies', 'NL West', 'National League'),
+('Los Angeles Dodgers', 'NL West', 'National League'),
+('San Diego Padres', 'NL West', 'National League'),
+('San Francisco Giants', 'NL West', 'National League');
